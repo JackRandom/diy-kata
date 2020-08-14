f3c51eb329388008929e2d193ecd325d23b5b44b
JackRandom <andrewglogan1987@gmail.com>
2020-08-14 18:25:58 +0100

    kata 5 complete

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,10 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) => {
+
+
+    let travelTime = distance / speed; 
+    result = (Math.round(travelTime * 2) / 2).toFixed(1);
+    
+    return result;
+};
 
 module.exports = reachDestination;
--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -3,6 +3,10 @@ const { reachDestination } = require("../src");
 describe("reachDestination", () => {
   test("returns string with estimated time of arrival", () => {
 
-    expect(reachDestination(44, 10)).toBe(4.5)
+    expect(reachDestination(44, 10)).toBe("4.5")
+  });
+  test("returns string with estimated time of arrival", () => {
+
+    expect(reachDestination(56, 10)).toBe("5.5")
   });
 });
